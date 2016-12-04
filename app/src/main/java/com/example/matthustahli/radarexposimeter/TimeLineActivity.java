e3b2e17b0499d5bde5e91901e4ea562b29d7034c
matthustahli <mstaehli@ethz.ch>
2016-12-04 12:58:20 +0100

    first try to show values in textview

--- app/src/main/java/com/example/matthustahli/radarexposimeter/TimeLineActivity.java
+++ app/src/main/java/com/example/matthustahli/radarexposimeter/TimeLineActivity.java
@@ -35,8 +35,8 @@ import static java.lang.Math.round;
 
 public class TimeLineActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Integer anzahlBalken=40;
-    Button b_modeNormal,b_mode21dB, b_mode42dB,b_mode_accumulation,b_startStop;
+    Integer anzahlBalken = 40;
+    Button b_modeNormal, b_mode21dB, b_mode42dB, b_mode_accumulation, b_startStop;
     ImageButton b_settings;
     TextView tv_status;
     LinearLayout settings;
@@ -55,15 +55,15 @@ public class TimeLineActivity extends AppCompatActivity implements View.OnClickL
     Timer timer;
     Runnable runnable;
     Handler handler;
-    int counter=0;
+    int counter = 0;
     //int size;
 
     //variables for plot
     Rectangle LargePeakBars, SmallRMSBars;
     Display display;
-    int colorFix, colorBar, colorActive, colorLimit, colorEmpty;
-    double abstandZwischenBalken =5.0; //5dp
-    Paint paintFix, paintBar, paintActive, paintLimit,paintEmpty;
+    int colorFix, colorBar, colorActive, colorLimit, colorEmpty, activeBar = 0;
+    double abstandZwischenBalken = 5.0; //5dp
+    Paint paintFix, paintBar, paintActive, paintLimit, paintEmpty;
     TextView TVMaxValue, TVMinValue, TVMiddleValue;
     ImageView imageView;
     Bitmap bitmap;
@@ -72,14 +72,15 @@ public class TimeLineActivity extends AppCompatActivity implements View.OnClickL
     double scaleY = 0.9;
     double scaleX = 0.9;
     double minPlot, maxPlot;
-    float lastValuePeak,lastValueRms,maxHight;
+    float lastValuePeak, lastValueRms, maxHight;
+    float[] AllPlotValuesPeak, AllPlotValuesRms;
+    double[] rmsValues, peakValues;
 
     //valiables for data exchange
     private final static char measurement_type = 'A';
     int freq;//frequencies are in MHz  //beinhaltet die zu betrachtenden frequenzen    //make switch funktion that deletes element at certain place and reorders them
     double rms;
     double peak;
-
 
 
     @Override
@@ -95,12 +96,12 @@ public class TimeLineActivity extends AppCompatActivity implements View.OnClickL
     }
 
     public void onStart() {
-        Log.d(LOG_TAG , "onStart called");
+        Log.d(LOG_TAG, "onStart called");
         super.onStart();
     }
 
-    public void onStop(){
-        Log.d(LOG_TAG , "onStop called");
+    public void onStop() {
+        Log.d(LOG_TAG, "onStop called");
         super.onStop();
     }
 
@@ -125,11 +126,11 @@ public class TimeLineActivity extends AppCompatActivity implements View.OnClickL
     public void getSettingsFromIntent() {
         Intent intent = getIntent();
         myMode = intent.getStringExtra("myMode");
-        if (myMode == "-21 dB")  attenuator = 1;
-        else if (myMode == "LNA on")  attenuator = 3;
-        else if(myMode == "normal mode")   attenuator = 0;
+        if (myMode == "-21 dB") attenuator = 1;
+        else if (myMode == "LNA on") attenuator = 3;
+        else if (myMode == "normal mode") attenuator = 0;
         else attenuator = 2;
-        freq = intent.getIntExtra("frequency",0);
+        freq = intent.getIntExtra("frequency", 0);
         //freq = 500 + freq*100;        //freq = value of freq MHz;
         Log.d("Timeline freq", String.valueOf(freq));
         modeMaxSize();
@@ -170,45 +171,52 @@ public class TimeLineActivity extends AppCompatActivity implements View.OnClickL
     }*/
 
 
-
-    public void modeMaxSize(){
-        int maxSizeInVolt=0;
-        switch (myMode){
+    public void modeMaxSize() {
+        int maxSizeInVolt = 0;
+        switch (myMode) {
             case "normal mode":
-                maxSizeInVolt=50;
+                maxSizeInVolt = 50;
                 break;
             case "-21 dB":
-                maxSizeInVolt=500;
+                maxSizeInVolt = 500;
                 break;
             case "-42 dB":
-                maxSizeInVolt=5000;
+                maxSizeInVolt = 5000;
                 break;
             case "LNA on":
-                maxSizeInVolt=5;
+                maxSizeInVolt = 5;
                 break;
         }
-        maxHight= (float) log(maxSizeInVolt);
+        maxHight = (float) log(maxSizeInVolt);
     }
 
     public void SetUpValuesForPlot() {
         qickFixArray = new double[anzahlBalken];
-        Arrays.fill(qickFixArray,0);
+        Arrays.fill(qickFixArray, 0);
         display = getWindowManager().getDefaultDisplay();
         size = new Point();
         display.getSize(size);
         imageView = (ImageView) findViewById(R.id.imageView_timeline_bitmap);
         bitmap = Bitmap.createBitmap(size.x, size.y, Bitmap.Config.ARGB_8888);
+        AllPlotValuesPeak = new float[anzahlBalken];
+        AllPlotValuesRms = new float[anzahlBalken];
+        rmsValues =new double[anzahlBalken];
+        peakValues= new double[anzahlBalken];
+        Arrays.fill(AllPlotValuesPeak, size.y);
+        Arrays.fill(AllPlotValuesRms, size.y);
+        Arrays.fill(rmsValues,0);
+        Arrays.fill(peakValues,0);
         paintFix = new Paint();
         paintBar = new Paint();
-        paintActive= new Paint();
-        paintLimit= new Paint();
-        paintEmpty= new Paint();
+        paintActive = new Paint();
+        paintLimit = new Paint();
+        paintEmpty = new Paint();
         colorFix = TimeLineActivity.this.getResources().getColor(R.color.fixedBar);
         colorBar = TimeLineActivity.this.getResources().getColor(R.color.normalBar);
         colorActive = TimeLineActivity.this.getResources().getColor(R.color.activeBar);
         colorLimit = TimeLineActivity.this.getResources().getColor(R.color.limitBar);
         //colorEmpty= Color.WHITE;
-        colorEmpty =TimeLineActivity.this.getResources().getColor(R.color.background);
+        colorEmpty = TimeLineActivity.this.getResources().getColor(R.color.background);
         paintEmpty.setColor(colorEmpty);
         paintEmpty.setStyle(Paint.Style.FILL);
         paintFix.setColor(colorFix);
@@ -220,54 +228,65 @@ public class TimeLineActivity extends AppCompatActivity implements View.OnClickL
         paintActive.setStyle(Paint.Style.FILL);
         canvas = new Canvas(bitmap);
         LargePeakBars = new Rectangle(anzahlBalken, abstandZwischenBalken, size.x, size.y, qickFixArray, myMode, scaleX, scaleY);
-        SmallRMSBars = new Rectangle(anzahlBalken, abstandZwischenBalken+20, size.x, size.y, qickFixArray, myMode, scaleX, scaleY);
+        SmallRMSBars = new Rectangle(anzahlBalken, abstandZwischenBalken + 20, size.x, size.y, qickFixArray, myMode, scaleX, scaleY);
         TVMaxValue = (TextView) findViewById(R.id.tv_maxValueScale);
         TVMiddleValue = (TextView) findViewById(R.id.tv_middleValueScale);
         TVMinValue = (TextView) findViewById(R.id.tv_minValueScale);
         SetPositionOnScreenOfTextViews();
     }
 
-    private void SetPositionOnScreenOfTextViews(){
-        ViewGroup.MarginLayoutParams top = (ViewGroup.MarginLayoutParams)TVMaxValue.getLayoutParams();
-        top.topMargin =(int) (size.y*(1.0-scaleY));
-        ViewGroup.MarginLayoutParams middle = (ViewGroup.MarginLayoutParams)TVMiddleValue.getLayoutParams();
-        middle.topMargin =(int) (size.y/2.0 + size.y*(1.0-scaleY)/2.0);
-        ViewGroup.MarginLayoutParams bottom = (ViewGroup.MarginLayoutParams)TVMinValue.getLayoutParams();
-        bottom.topMargin = (int) (size.y -size.y*(1.0-scaleY));
+    private void SetPositionOnScreenOfTextViews() {
+        ViewGroup.MarginLayoutParams top = (ViewGroup.MarginLayoutParams) TVMaxValue.getLayoutParams();
+        top.topMargin = (int) (size.y * (1.0 - scaleY));
+        ViewGroup.MarginLayoutParams middle = (ViewGroup.MarginLayoutParams) TVMiddleValue.getLayoutParams();
+        middle.topMargin = (int) (size.y / 2.0 + size.y * (1.0 - scaleY) / 2.0);
+        ViewGroup.MarginLayoutParams bottom = (ViewGroup.MarginLayoutParams) TVMinValue.getLayoutParams();
+        bottom.topMargin = (int) (size.y - size.y * (1.0 - scaleY));
 
     }
 
-    synchronized private void makePlot(){
-        if(makePlotRunning ==true){
-            int next = counter %anzahlBalken;
-            lastValuePeak= (float) readPeak();
-            lastValueRms= (float) readRMS();
+    synchronized private void makePlot() {
+        if (makePlotRunning == true) {
+            int next = counter % anzahlBalken;
+            peakValues[next] = readPeak();
+            rmsValues[next] = readRMS();
+            lastValuePeak = (float) peakValues[next];
+            lastValueRms = (float) rmsValues[next];
             Log.d("Timeline peak", String.valueOf(lastValuePeak));
             Log.d("Timeline rms", String.valueOf(lastValueRms));
             //delet bar first
             canvas.drawRect(LargePeakBars.getLeft(next), 0, LargePeakBars.getRight(next), LargePeakBars.getBottom(next), paintEmpty);
             //draw the bar
-            if(lastValuePeak<-1){
-                if(lastValuePeak<-2.5){}
-                else{
-                    lastValuePeak = (float) (size.y- size.y* scaleY); //full size
+            if (lastValuePeak < -1) {
+                if (lastValuePeak < -2.5) {
+                    lastValuePeak = size.y;
+                    lastValueRms = size.x;
+                } else {
+                    lastValuePeak = (float) (size.y - size.y * scaleY); //full size
                     canvas.drawRect(LargePeakBars.getLeft(next), lastValuePeak, LargePeakBars.getRight(next), LargePeakBars.getBottom(next), paintLimit);
                 }
-            }else{
-                lastValuePeak = (float) (size.y - log(lastValuePeak)/maxHight*size.y*scaleY);
+            } else {
+                lastValuePeak = (float) (size.y - log(lastValuePeak) / maxHight * size.y * scaleY);
                 canvas.drawRect(LargePeakBars.getLeft(next), lastValuePeak, LargePeakBars.getRight(next), LargePeakBars.getBottom(next), paintBar);
-                lastValueRms = (float) (size.y - log(lastValueRms)/maxHight*size.y*scaleY);
+                lastValueRms = (float) (size.y - log(lastValueRms) / maxHight * size.y * scaleY);
                 canvas.drawRect(SmallRMSBars.getLeft(next), lastValueRms, SmallRMSBars.getRight(next), SmallRMSBars.getBottom(next), paintActive);
             }
+            AllPlotValuesPeak[next] = lastValuePeak;
+            AllPlotValuesRms[next] = lastValueRms;
             imageView.setImageBitmap(bitmap);
             counter++;
-        }else{}
+        } else {
+        }
     }
 
-    synchronized private void resetPlotToBeginning(){
+    synchronized private void resetPlotToBeginning() {
+        Arrays.fill(AllPlotValuesPeak, size.y);
+        Arrays.fill(AllPlotValuesRms, size.y);
+        Arrays.fill(rmsValues,0);
+        Arrays.fill(peakValues,0);
         canvas.drawColor(Color.WHITE);
         imageView.setImageBitmap(bitmap);
-        counter=0;
+        counter = 0;
     }
 
     private void initalizeButtonsAndIcons() {
@@ -293,7 +312,7 @@ public class TimeLineActivity extends AppCompatActivity implements View.OnClickL
     @Override
     public void onClick(View v) {
         Timeline_Packet_Trigger timeStop = new Timeline_Packet_Trigger(device_id, attenuator, freq, (char) 0);
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.b_mode_normal:
                 myMode = "normal mode";
                 attenuator = 0;
@@ -346,34 +365,21 @@ public class TimeLineActivity extends AppCompatActivity implements View.OnClickL
                 }
                 break;
             case R.id.startStopButton:
-                if(makePlotRunning ==true){
+                if (makePlotRunning == true) {
                     //stop
                     makePlotRunning = false;
                     b_startStop.setText("Start");
-                }else{
+                } else {
                     makePlotRunning = true;
                     //start
                     b_startStop.setText("Stop");
                 }
                 break;
-                //set variable on false
-
-               /* if(measurement_type == 'R'){
-                    measurement_type = 'P';
-                    tv_status.setText("Peak");
-                    b_switchMode.setText("RMS");
-                    makePlot();
-                }else{
-                    measurement_type = 'R';
-                    tv_status.setText("RMS");
-                    b_switchMode.setText("Peak");
-                    makePlot();
-                }*/
         }
     }
 
+    //this is for closing settings and registrating touch on plot
     public void ActivateTouchOnPlot() {
-
         View touchView = findViewById(R.id.activity_time_line);
         touchView.setOnTouchListener(new View.OnTouchListener() {
 
@@ -385,20 +391,79 @@ public class TimeLineActivity extends AppCompatActivity implements View.OnClickL
                             if (settings.getVisibility() != LinearLayout.GONE) {
                                 settings.setVisibility(LinearLayout.GONE);
                             }
-                        } catch (NullPointerException e) {}
-                        //int position = returnPosition((int) event.getX());
-                        //changeBarColorToActiv(position);
+                        } catch (NullPointerException e) {
+                        }
+                        int position = returnPosition((int) event.getX());
+                        changeBarColorToActiv(position);
                         break;
                     }
                     case MotionEvent.ACTION_MOVE: {
-                        //int position = returnPosition((int) event.getX());
-                        //changeBarColorToActiv(position);
+                        int position = returnPosition((int) event.getX());
+                        changeBarColorToActiv(position);
                         break;
                     }
                 }
                 return true; //true= we handled the event!!
             }
         });
+    }
+
+    //gets position from coordinates
+    public int returnPosition(int x) {
+        int i = 0;
+        int fromLeftToRight = (int) LargePeakBars.getLeft(i);
+        while (fromLeftToRight < x) {
+            if (i == anzahlBalken - 1) {        //boundary condition on right edge
+                return i;
+            }
+            i++;
+            fromLeftToRight = (int) LargePeakBars.getLeft(i);
+        }
+        return i;
+    }
+
+    public void changeBarColorToActiv(int position) {
+        //desactivate last visited bar
+        changeBarColorToNOTactiv(activeBar);
+        activeBar = position;       //with this position we can also use the add button to put it in a list!
+        updateTextViewsShowingValues(position); //update textview
+        //set color to active
+        canvas.drawRect(LargePeakBars.getLeft(position), AllPlotValuesPeak[position], LargePeakBars.getRight(position), LargePeakBars.getBottom(position), paintFix);
+        imageView.setImageBitmap(bitmap);
+    }
+
+    //changes Bar back to normal plot color
+    private void changeBarColorToNOTactiv(Integer position) {
+        canvas.drawRect(LargePeakBars.getLeft(position), AllPlotValuesPeak[position], LargePeakBars.getRight(position), LargePeakBars.getBottom(position), paintBar);
+        canvas.drawRect(SmallRMSBars.getLeft(position), AllPlotValuesRms[position], SmallRMSBars.getRight(position), SmallRMSBars.getBottom(position), paintActive);
+        imageView.setImageBitmap(bitmap);
+    }
+
+    private void updateTextViewsShowingValues(int position) {
+        if (rmsValues[position] < -1) {
+            if (rmsValues[position] < -2.5) {
+                //selectedValue.setText("< min ");
+            } else {
+                //selectedValue.setText("> max ");
+            }
+        } else {
+            double valueToShow = rmsValues[position];
+            valueToShow = Math.round(valueToShow * 100);  // runden auf ##.#
+            valueToShow = valueToShow / 100;
+            //selectedValue.setText(String.valueOf(valueToShow)+ " V/m ");
+        }
+        if (peakValues[position] < -1) {
+            if (peakValues[position] < -2.5) {
+                // selectedValue.setText("< min ");
+            } else {
+                // selectedValue.setText("> max ");
+            }
+        } else {
+            double valueToShow = peakValues[position];
+            valueToShow = Math.round(valueToShow * 100);  // runden auf ##.#
+            valueToShow = valueToShow / 100;
+            //selectedValue.setText(String.valueOf(valueToShow) + " V/m ");
+        }
     }
 
     //handles the allert bar, for example when connection is lost.
