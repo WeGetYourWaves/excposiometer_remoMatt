1f11a62466543fe4d03b2c46c0cc7298b6902f01
aeggli <aeggli@users.noreply.github.com>
2016-12-04 19:49:08 +0100

    Code aufgeräumt

--- app/src/main/java/com/example/matthustahli/radarexposimeter/CommunicationService.java
+++ app/src/main/java/com/example/matthustahli/radarexposimeter/CommunicationService.java
@@ -1,5 +1,24 @@
 package com.example.matthustahli.radarexposimeter;
 
+// CommunicationService does:
+// 1. Turn off Wifi
+// 2. Turn Hotspot on
+// 3. start an instance of TCP_SERVER (an Interface)
+// 4. Pass TrigerPackages (from BroadcastReceiver) to the TCP_SERVER (via WifiDataBuffer)
+// 5. Send measurement-data (from WifiDataBuffer) back to an Activity (via BroadcastSender)
+// 6. Stop itself when stopservice(intent) is called by an activity
+// 7. Turn the hotspot off
+// 8. Turn Wifi back on, if it was on on startup of service
+
+/**
+ * Created by André Eggli on 20.10.16.
+ */
+
+// Source: http://android-coding.blogspot.ch/2011/11/interactive-between-activity-and.html
+// Source: http://stackoverflow.com/questions/13124115/starting-android-service-already-running
+// Source: http://stackoverflow.com/questions/6394599/android-turn-on-off-wifi-hotspot-programmatically
+
+
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -11,33 +30,20 @@ import android.os.IBinder;
 import org.jetbrains.annotations.Nullable;
 import android.util.Log;
 import android.widget.Toast;
-
 import java.lang.reflect.Method;
 
-import static java.lang.Math.pow;
 
-/**
- * Created by André Eggli on 20.11.16.
- */
-
-// Quelle: http://android-coding.blogspot.ch/2011/11/interactive-between-activity-and.html
-// Quelle: http://stackoverflow.com/questions/13124115/starting-android-service-already-running
 public class CommunicationService extends Service {
 
     WifiManager wifi_manager;
     Boolean WifiWasOnWhenServiceWasStarted = false;
     static final WifiDataBuffer wifiDataBuffer = new WifiDataBuffer();
-    TCP_Data_dequeue_Thread TCP_Data_Sender = new TCP_Data_dequeue_Thread();
-    byte[] callipack;
-
+    TCP_Data_dequeue_Thread dataSenderThread = new TCP_Data_dequeue_Thread();
     final static TCP_SERVER Socket = new Fake_TCP_Server(wifiDataBuffer); // Initialise Fake TCP to test
     // final static TCP_SERVER Socket = new TCPServer(wifiDataBuffer); // Initialise real TCP_Server to test ESP8266
     // final static TCP_SERVER Socket = new Excel_Facke_TCP_Server(wifiDataBuffer);
-
     IntentListenerForActivity ListenerForActivity; // receives Data from Activity via Broadcast
-
     private static final String LOG_TAG = "Service";
-
     public static final String ACTION_FROM_ACTIVITY = "ACTION_FROM_ACTIVITY";
     public static final String TRIGGER_Serv2Act = "Service -> Activity";
     public static final String COMMAND_Act2Serv = "COMMAND_Act2Serv";
@@ -45,20 +51,18 @@ public class CommunicationService extends Service {
     public static final int CMD_getCALI = 2;
     public static final String TRIGGER_Act2Serv = "Activity -> Service";
     public static final String DATA_BACK = "DATA_BACK";
-
     boolean running = true;
-
-    byte[] initData;
+    byte[] callipack;
 
     public CommunicationService() {
         Log.d(LOG_TAG, "Constructor called");
-        TCP_Data_Sender.start(); // dequeues Data from TCPServer and Broadcasts it to Activity
+        dataSenderThread.start(); // dequeues Data from TCPServer and Broadcasts it to Activity
     }
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        // Is never used
+        // Is never used but has to be implemented
         Log.d(LOG_TAG, "onBind called");
         Toast.makeText(this, "onBind called", Toast.LENGTH_SHORT).show();
         return null;
@@ -87,26 +91,9 @@ public class CommunicationService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(LOG_TAG, "onStartCommand");
-
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(ACTION_FROM_ACTIVITY);
         registerReceiver(ListenerForActivity, intentFilter);
-
-//        wifi_manager = (WifiManager) this.getSystemService(this.WIFI_SERVICE);
-//        WifiConfiguration wifi_configuration = null;
-//        wifi_manager.setWifiEnabled(false);
-//        try {
-//            // Source http://stackoverflow.com/questions/13946607/android-how-to-turn-on-hotspot-in-android-programmatically
-//            Method method = wifi_manager.getClass().getMethod("setWifiApEnabled", WifiConfiguration.class, boolean.class);
-//            method.invoke(wifi_manager, wifi_configuration, true);
-//            Log.d(LOG_TAG,"in onStartCommand, turned Hotspot on");
-//        } catch (Exception e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-
-        // Toast.makeText(this, "Service started", Toast.LENGTH_SHORT).show();
-
         super.onStartCommand(intent, flags, startId);
         return START_NOT_STICKY;
     }
@@ -135,39 +122,15 @@ public class CommunicationService extends Service {
         intent.setAction(TRIGGER_Serv2Act);
         intent.putExtra(DATA_BACK, data);
         sendBroadcast(intent);
-
     }
 
-    protected byte[] split_packet (int start, int end, byte[] packet){
-
+    private byte[] split_packet (int start, int end, byte[] packet){
         int length = end - start + 1;
         byte[] splitted = new byte[length];
         for (int i = 0; i < length; i++){
             splitted[i] = packet[i + start];
         }
-
         return splitted;
-    }
-
-    private int byteArray2int (byte[] byteArray){
-        int Integr = 0;
-        if (byteArray.length == 1) {
-            Integr = byteArray[0] & 0xFF;
-        }
-        else if (byteArray.length == 2){
-            Integr = (int) ((byteArray[0] & 0xFF) * pow(2, 8));
-            Integr += byteArray[1] & 0xFF;
-        }
-        else if (byteArray.length == 4){
-            Integr = (int) ((byteArray[0] & 0xFF) * pow(2, 24));
-            Integr += (int) ((byteArray[1] & 0xFF) * pow(2, 16));
-            Integr += (int) ((byteArray[2] & 0xFF) * pow(2, 8));
-            Integr += byteArray[1] & 0xFF;
-        }
-        else {
-            throw new IllegalArgumentException("byteArray must have length 1,2 or 3");
-        }
-        return Integr;
     }
 
     public class TCP_Data_dequeue_Thread extends Thread{
@@ -175,7 +138,7 @@ public class CommunicationService extends Service {
         @Override
         public void run() {
             Log.d(Log_tag, "Thread started");
-            while(running){ // && !TCP_Data_Sender.isInterrupted()
+            while(running){ // && !dataSenderThread.isInterrupted()
                 // Log.d(LOG_TAG, "Thread runns");
                 try {
                     if(!wifiDataBuffer.isDataWaiting_FromESP()){
@@ -192,12 +155,13 @@ public class CommunicationService extends Service {
                         }
                     }
                 } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
             }
+            // case 'running == false'
             Log.d(Log_tag, "Thread ended, going to call onDestroy of Service");
             stopSelf(); // Stop the Service
+            // onDestroy of Service is called to restore WifiSettings
         }
     }
 
@@ -218,7 +182,6 @@ public class CommunicationService extends Service {
             else if (intent.hasExtra(TRIGGER_Act2Serv)){
                 byte[] TriggerPack = intent.getByteArrayExtra(TRIGGER_Act2Serv);
                 wifiDataBuffer.enqueue_ToESP(TriggerPack);
-                initData = TriggerPack;
             }
         }
     }
